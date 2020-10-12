1e002e4a14d7cd3484358e919cf5f990c7fc9b1a
Robert Soriano <44214671+robertomsoriano@users.noreply.github.com>
2020-10-12 19:45:46 -0400

    . .

--- routes/api/cartItems.js
+++ routes/api/cartItems.js
@@ -1,115 +1,138 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-
+// Item Model (to check stock)
+const Item = require("../../models/Item");
 // CartItem Model
 const CartItem = require("../../models/CartItem");
+
+// Helper Functions
+// Check If Enough Items in Stock
+const checkStock = async (itemID, quantityRequested) => {
+  let itemToCheck = await Item.findById(itemID);
+  let quantityInCart = await CartItem.findById(itemID);
+  console.log(
+    await `${itemToCheck.item_quantity}---------- ${quantityInCart.item_quantity}`
+  );
+  return (await itemToCheck.item_quantity) >= quantityRequested + quantityInCart
+    ? true
+    : false;
+};
 
 // @route   GET api/cart-items
 // @desc    Get all cartItems
 // @access  Private
 router.get("/", auth, (req, res) => {
-    console.log(`CartItem GET Req at ${Date()}`);
-    try {
-        CartItem.find({ user: req.user.id })
-            .sort({ date_added: -1 })
-            .then(items => res.send(items));
-    } catch (e) {
-        return res.status(400).json({ msg: "User's cart is empty" });
-    }
+  console.log(`CartItem GET Req at ${Date()}`);
+  try {
+    CartItem.find({ user: req.user.id })
+      .sort({ date_added: -1 })
+      .then((items) => res.send(items));
+  } catch (e) {
+    return res.status(400).json({ msg: "User's cart is empty" });
+  }
 });
-
-
 
 // @route   PUT api/cart-items/add
 // @desc    Add item to cart (or inc quantity)
 // @access  Private
 router.put("/add", auth, async (req, res) => {
-    // If item is already in the Cart, quantity will increase.
-    //Else, item will be added.
-    try {
-        await CartItem.findOneAndUpdate(
-            { _id: req.body.item.item_id, user: req.user.id },
-            {
-                $inc: { item_quantity: 1 },
-                item_name: req.body.item.item_name,
-                item_price: req.body.item.item_price,
-                item_type: req.body.item.item_type,
-                item_image: req.body.item.item_image,
-            },
-            {
-                new: true,
-                upsert: true
-            }
-        );
-        return CartItem.find({ user: req.user.id }, (err, cart) => {
-            res.send(cart);
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(404).json({ msg: false });
+  console.log(`Cart Item Increase Requested.`);
+  // If item is already in the Cart, quantity will increase.
+  //Else, item will be added.
+  try {
+    // Check if enough stock first
+    if (
+      (await checkStock(req.body.item.item_id, req.body.item.item_quantity)) ===
+      true
+    ) {
+      console.log(
+        await checkStock(req.body.item.item_id, req.body.item.item_quantity)
+      );
+      await CartItem.findOneAndUpdate(
+        { _id: req.body.item.item_id, user: req.user.id },
+        {
+          $inc: { item_quantity: 1 },
+          item_name: req.body.item.item_name,
+          item_price: req.body.item.item_price,
+          item_type: req.body.item.item_type,
+          item_image: req.body.item.item_image
+        },
+        {
+          new: true,
+          upsert: true
+        }
+      );
+      return CartItem.find({ user: req.user.id }, (err, cart) => {
+        res.send(cart);
+      });
+    } else {
+      throw new Error("Not Enough Stock");
     }
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ msg: false });
+  }
 });
-
 
 // @route   PUT api/cart-items/decrease
 // @desc    Decrease item to cart (or dec quantity)
 // @access  Private
 router.put("/decrease", auth, async (req, res) => {
-    // Item quantity will decrease.
-    //If it falls below one, it will be deleted.
-    try {
-        await CartItem.findOneAndUpdate(
-            { _id: req.body.item.item_id, user: req.user.id },
-            {
-                $inc: { item_quantity: -1 },
-                item_name: req.body.item.item_name,
-                item_price: req.body.item.item_price,
-                item_type: req.body.item.item_type,
-                item_image: req.body.item.item_image,
-            },
-            {
-                new: true,
-                upsert: true
-            }
-        );
-        return CartItem.find({ user: req.user.id }, (err, cart) => {
-            res.send(cart);
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(404).json({ msg: false });
-    }
+  console.log(`Cart Item Decrease Requested.`);
+  // Item quantity will decrease.
+  //If it falls below one, it will be deleted.
+  try {
+    await CartItem.findOneAndUpdate(
+      { _id: req.body.item.item_id, user: req.user.id },
+      {
+        $inc: { item_quantity: -1 },
+        item_name: req.body.item.item_name,
+        item_price: req.body.item.item_price,
+        item_type: req.body.item.item_type,
+        item_image: req.body.item.item_image
+      },
+      {
+        new: true,
+        upsert: true
+      }
+    );
+    return CartItem.find({ user: req.user.id }, (err, cart) => {
+      res.send(cart);
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ msg: false });
+  }
 });
-
 
 // @route   DELELTE api/cart-items
 // @desc    Remove item from cart
 // @access  Private
 router.put("/delete-item", auth, async (req, res) => {
-    try {
-        await CartItem.findById(req.body.item.item_id).then(item =>
-            item.remove().then(() =>
-                CartItem.find({ user: req.user.id }, (err, cart) => {
-                    res.send(cart);
-                })
-            )
-        );
-    } catch (err) {
-        res.status(404).json({ msg: false });
-    }
+  try {
+    await CartItem.findById(req.body.item.item_id).then((item) =>
+      item.remove().then(() =>
+        CartItem.find({ user: req.user.id }, (err, cart) => {
+          res.send(cart);
+        })
+      )
+    );
+  } catch (err) {
+    res.status(404).json({ msg: false });
+  }
 });
 
 // @route   DELELTE api/cart-items/empty
 // @desc    empty Cart
 // @access  Private
 router.post("/empty", auth, async (req, res) => {
-    try {
-        await CartItem.deleteMany({ user: req.user.id });
-        return res.json({ success: true, deleted: true });
-    } catch (err) {
-        res.status(404).json({ msg: false });
-    }
+  try {
+    await CartItem.deleteMany({ user: req.user.id });
+    return res.json({ success: true, deleted: true });
+  } catch (err) {
+    res.status(404).json({ msg: false });
+  }
 });
 
 module.exports = router;
--- routes/api/itemTransactions.js
+++ routes/api/itemTransactions.js
@@ -3,46 +3,83 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const ItemTransaction = require("../../models/ItemTransaction");
 const Item = require("../../models/Item");
-
+const CartItem = require("../../models/CartItem");
 // @route   POST /item-checkout
 // @desc    Process item-transactions
 // @access  Private
 
 router.post("/", auth, (req, res) => {
-    // Checkout submission will log a trasaction
-    const newTransaction = new ItemTransaction({
-        transac_type: req.body.transaction.transac_type,
-        transac_operator: req.body.transaction.transac_operator,
-        // Don't required customer data for now
-        transac_customer: req.body.transaction.transac_customer,
-        transac_items: req.body.transaction.transac_items,
-        sale_transac: req.body.transaction.sale_transac,
-        transac_subtotal: req.body.transaction.transac_subtotal,
-        transac_discount: req.body.transaction.transac_discount,
-        transac_taxes: req.body.transaction.transac_taxes,
-        transac_total: req.body.transaction.transac_total,
-        amount_received: req.body.transaction.amount_received,
-        transac_message: req.body.transaction.transac_message,
-        user: req.user.id
+  // Checkout submission will log a trasaction
+  let newTransaction = new ItemTransaction({
+    transac_type: req.body.transaction.transac_type,
+    transac_operator: req.body.transaction.transac_operator,
+    // Don't required customer data for now
+    transac_customer: req.body.transaction.transac_customer,
+    transac_items: req.body.transaction.transac_items,
+    sale_transac: req.body.transaction.sale_transac,
+    transac_subtotal: req.body.transaction.transac_subtotal,
+    transac_discount: req.body.transaction.transac_discount,
+    transac_taxes: req.body.transaction.transac_taxes,
+    transac_total: req.body.transaction.transac_total,
+    amount_received: req.body.transaction.amount_received,
+    transac_message: req.body.transaction.transac_message,
+    transac_status: "reconciled",
+    user: req.user.id
+  });
+  try {
+    req.body.transaction.transac_items.map((itemProcessing) => {
+      Item.findById(itemProcessing._id, (err, ItemToUpdate) => {
+        ItemToUpdate.item_quantity =
+          ItemToUpdate.item_quantity - itemProcessing.item_quantity;
+        ItemToUpdate.save();
+      });
     });
-    try {
-        // Check Inventory first
-        req.body.transaction.itemsToUpdate.map(itemProcessing => {
-            Item.findById(itemProcessing.id, (err, ItemToUpdate) => {
-                ItemToUpdate.item_quantity = ItemToUpdate.item_quantity - itemProcessing.item_quantity;
-                ItemToUpdate.save();
-            });
-        });
-        // If there are enough items in stock process transaction
-        newTransaction.save().then(trans => res.json(trans));
-        return;
-    } catch (err) {
-
-        return res.status(425).json({
-            msg: "Transaction not valid. Are you ordering more books than available?"
-        });
-    }
+    newTransaction.save().then((trans) => res.json(trans));
+    return;
+  } catch (err) {
+    return res.status(425).json({
+      msg: "Transaction not valid. Are you ordering more books than available?"
+    });
+  }
 });
 
+// @route   PUT /item-checkout
+// @desc    Revert item-transactions
+// @access  Private
+
+router.put("/revert/:invoice_number", auth, (req, res) => {
+  // Checkout submission will log a trasaction
+  let transactionObj = ItemTransaction.findOne({
+    invoice_number: req.params.invoice_number
+  });
+  try {
+    // Re-stock Inventory
+    req.body.transaction.transac_items.map((itemProcessing) => {
+      Item.findById(itemProcessing._id, (err, ItemToUpdate) => {
+        ItemToUpdate.item_quantity =
+          ItemToUpdate.item_quantity + itemProcessing.item_quantity;
+        ItemToUpdate.save();
+      });
+    });
+    // Set transaction as voided and reverted
+    transactionObj.transac_status = "voided";
+    transactionObj.save().then((trans) => res.json(trans));
+    return;
+  } catch (err) {
+    return res.status(425).json({
+      msg: "Transaction not valid. Are you ordering more books than available?"
+    });
+  }
+});
+
+// @route   POST api/item-checkout
+// @desc    GET user all  Transactions
+// @access  Private
+router.get("/", (req, res) => {
+  // ItemTransaction.find({ user: req.user.id })
+  ItemTransaction.find()
+    .sort({ invoice_number: -1 })
+    .then((trans) => res.send(trans));
+});
 
 module.exports = router;
