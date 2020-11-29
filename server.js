399eaa3f3b2516888c7c49c76199509cb12e4400
Roberto <robertmsoriano@gmail.com>
2020-11-28 20:51:28 -0600

    Run Fronend Build

--- server.js
+++ server.js
@@ -82,13 +82,13 @@ app.use("/api/vehicles", require("./routes/api/vehicles"));
 app.use("/api/cart-items", require("./routes/api/cartItems"));
 app.use("/api/item-checkout", require("./routes/api/itemTransactions"));
 // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
